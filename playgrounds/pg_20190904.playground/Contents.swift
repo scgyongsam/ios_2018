082c27f20742444e11790e0ccb1d6650c229d063
GyonG sam <scgyongsam@gmail.com>
2018-09-08 05:59:57 +0900

    test

--- playgrounds/pg_20190904.playground/Contents.swift
+++ playgrounds/pg_20190904.playground/Contents.swift
@@ -1,22 +1,27 @@
 //: Playground - noun: a place where people can play
 
+// Language = Swift
+
 import Foundation
 
-//    .::::::..::    .   .::::::.-:::::'::::::::::::       .::    :.
-//   ;;;`    `';;,  ;;  ;;;' ;;;;;;'''' ;;;;;;;;''''     ,;';;    ;;
-//   '[==/[[[[,'[[, [[, [['  [[[[[[,,==      [[         ,[' [[    [[
-//     '''    $  Y$c$$$c$P   $$$`$$$"``      $$         $P__$$c   $$
-//    88b    dP   "88"888    888 888         88,        `"""88"d8b88
-//     "YMmMY"     "M "M"    MMM "MM,        MMM            MM YMPMM
-//    .-:::::'   ...    :::::::..       :::::::.  :::  .,-:::::/  ::::::.    :::.:::.    :::..,:::::: :::::::..   .::::::.
-//    ;;;'''' .;;;;;;;. ;;;;``;;;;       ;;;'';;' ;;;,;;-'````'   ;;;`;;;;,  `;;;`;;;;,  `;;;;;;;'''' ;;;;``;;;; ;;;`    `
-//    [[[,,==,[[     \[[,[[[,/[[['       [[[__[[\.[[[[[[   [[[[[[/[[[  [[[[[. '[[  [[[[[. '[[ [[cccc   [[[,/[[[' '[==/[[[[,
-//    `$$$"``$$$,     $$$$$$$$$c         $$""""Y$$$$$"$$c.    "$$ $$$  $$$ "Y$c$$  $$$ "Y$c$$ $$""""   $$$$$$c     '''    $
-//     888   "888,_ _,88P888b "88bo,    _88o,,od8P888 `Y8bo,,,o88o888  888    Y88  888    Y88 888oo,__ 888b "88bo,88b    dP
-//     "MM,    "YMMMMMP" MMMM   "W"     ""YUMMMP" MMM   `'YMUP"YMMMMM  MMM     YM  MMM     YM """"YUMMMMMMM   "W"  "YMmMY"
-//    .:::.          :. .::::.             .,,.                .::       .:::::::::::::...    :::.,::::::  :.
-//   ,;'``;.   ,;;,  ;;`;.  ,;'     ,;;, ,;;'`';,     ,;;,   ,;';;     ,;' ;;;;;;;;'''';;     ;;;;;;;''''   ';,
-//   ''  ,[[',['  [n [[  [nn[,    ,['  [n[[, _,[[   ,['  [n ,[' [[     n[       [[    [['     [[[ [[cccc     [n
-//   .c$$P'  $$    $$$$ $"   $c   $$    $$Y$$P"$$   $$    $$$P__$$c    Y$       $$    $$      $$$ $$""""     $Y
-//  d88 _,oo,Y8,  ,8"88 Yb,_,8Pd8bY8,  ,8",,_,d8"d8bY8,  ,8"`"""88"     8o,     88,   88    .d888 888oo,__ ,o8
-//  MMMUP*"^^ "YmmP  MM  "YMP" YMP "YmmP   "MP"  YMP "YmmP      MM       "M     MMM    "YmmMMMM"" """"YUMMMM"
+typealias Crew = String
+let janeway = "Some"
+
+struct Starship {
+    var name: String
+    var maxWarp: Double
+    var captain: Crew
+
+    func gotToMaximumWarp () {
+        print("\(name) is now travelling at warp \(maxWarp)")
+    }
+}
+
+let voyager = Starship(name: "Voyager", maxWarp: 9.975, captain: janeway)
+let nameKeyPath = \Starship.name//  ==> 여기의 의미에 대해서 좀 알려주세요!!
+
+let name = voyager[keyPath: nameKeyPath]
+name
+
+// Hashable protocol 을 conform 하는
+// String, Int, Double, Float 이런것들 쓸 수 있다.
